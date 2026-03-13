4a8cbcabe563770925005dbd6160c5879207af01
GitHub Action <notifications@github.com>
2026-03-13 14:47:22 +0000

    Automated Terraform Formatting

--- main.tf
+++ main.tf
@@ -2,32 +2,32 @@
 
 resource "github_repository" "this" {
 
-  name                                    = lower("terraform-${var.module_provider}-${var.module_name}")
-  description                             = "Terraform module to manage ${element(split("-", var.module_provider), 1)} resources."
-  homepage_url                            = var.homepage_url
-  visibility                              = var.visibility
-  has_issues                              = var.has_issues
-  has_discussions                         = var.has_discussions
-  has_projects                            = var.has_projects
-  has_wiki                                = var.has_wiki
-  is_template                             = var.is_template
-  allow_merge_commit                      = var.allow_merge_commit
-  allow_squash_merge                      = var.allow_squash_merge
-  allow_rebase_merge                      = var.allow_rebase_merge
-  allow_auto_merge                        = var.allow_auto_merge
-  squash_merge_commit_title               = var.squash_merge_commit_title
-  squash_merge_commit_message             = var.squash_merge_commit_message
-  merge_commit_title                      = var.merge_commit_title
-  merge_commit_message                    = var.merge_commit_message
-  delete_branch_on_merge                  = var.delete_branch_on_merge
-  auto_init                               = var.auto_init
-  gitignore_template                      = var.gitignore_template
-  license_template                        = var.license_template
-  archived                                = var.archived
-  archive_on_destroy                      = var.archive_on_destroy
-  topics                                  = concat(["terraform-module", "terraform", "terraform-managed"], var.topics)
-  vulnerability_alerts                    = var.vulnerability_alerts
-  allow_update_branch                     = var.allow_update_branch
+  name                        = lower("terraform-${var.module_provider}-${var.module_name}")
+  description                 = "Terraform module to manage ${element(split("-", var.module_provider), 1)} resources."
+  homepage_url                = var.homepage_url
+  visibility                  = var.visibility
+  has_issues                  = var.has_issues
+  has_discussions             = var.has_discussions
+  has_projects                = var.has_projects
+  has_wiki                    = var.has_wiki
+  is_template                 = var.is_template
+  allow_merge_commit          = var.allow_merge_commit
+  allow_squash_merge          = var.allow_squash_merge
+  allow_rebase_merge          = var.allow_rebase_merge
+  allow_auto_merge            = var.allow_auto_merge
+  squash_merge_commit_title   = var.squash_merge_commit_title
+  squash_merge_commit_message = var.squash_merge_commit_message
+  merge_commit_title          = var.merge_commit_title
+  merge_commit_message        = var.merge_commit_message
+  delete_branch_on_merge      = var.delete_branch_on_merge
+  auto_init                   = var.auto_init
+  gitignore_template          = var.gitignore_template
+  license_template            = var.license_template
+  archived                    = var.archived
+  archive_on_destroy          = var.archive_on_destroy
+  topics                      = concat(["terraform-module", "terraform", "terraform-managed"], var.topics)
+  vulnerability_alerts        = var.vulnerability_alerts
+  allow_update_branch         = var.allow_update_branch
 
   dynamic "pages" {
     for_each = var.pages != null ? [true] : []
